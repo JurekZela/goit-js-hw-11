0d31de01635ed7cd233f45916319fa8f6d6e62bf
JurekZela <122160360+JurekZela@users.noreply.github.com>
2023-09-30 19:06:26 +0200

    onError, search images

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-const ref = {
-    form: document.getElementById('search-form'),
-};
-
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { fetchImages } from './search_images-api';
 
-fetchImages()
-.then(Images => {
-    console.log(Images);
-})
-.catch(err => {
-    console.log(err);
-})
+const ref = {
+    form: document.getElementById('search-form'),
+    input: document.querySelector('input'),
+};
+
+ref.form.addEventListener('submit', addImagesFetch);
+
+ async function addImagesFetch(e) {
+    e.preventDefault();
+
+    try {
+        const inputValue = ref.input.value;
+        const resultFetch = await fetchImages(inputValue)
+        
+        if (resultFetch.total === 0) {
+            onError()
+            return;
+        }
+        console.log(resultFetch);
+    } catch {onError()}
+}
+
+function onError() {
+        Notify.failure("Sorry, there are no images matching your search query. Please try again.", {
+            position: 'center-center',
+        });
+};
--- src/search_images-api.js
+++ src/search_images-api.js
@@ -4,14 +4,11 @@ const api_key = axios.defaults.headers.common["x-api-key"] = "live_MXi5doRNd1frI
 const PIX_API_KEY = "39684416-19726dcb7b6323782764f8c99";
 const BASE_URL = 'https://pixabay.com/api/';
 
-export function fetchImages() {
-    const url = `${BASE_URL}?key=${PIX_API_KEY}`;
+async function fetchImages(searchTerm) {
+    const url = `${BASE_URL}?key=${PIX_API_KEY}&q=${searchTerm}&image_type=photo&orientation=horizontal&safesearch=true`;
 
-    return  fetch(url)
-    .then(response => {
-        if (!response.ok) {
-             throw new Error(response.status)
-        }
-        return response.json();
-    })
+    const resolve = await fetch(url);
+    return await resolve.json();
 };
+
+export { fetchImages };
