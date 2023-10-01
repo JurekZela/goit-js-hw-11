34145cc475fa7fe70942903b1ae9139e7cda1f37
JurekZela <122160360+JurekZela@users.noreply.github.com>
2023-10-01 19:06:03 +0200

    pagination

--- src/index.js
+++ src/index.js
@@ -3,50 +3,61 @@ import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+// const  lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250,})
+
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { fetchImages } from './search_images-api';
+
+let page = 0;
+let perPage = 40;
 
 const ref = {
     form: document.getElementById('search-form'),
     input: document.querySelector('input'),
     gallery: document.querySelector('.gallery'),
+    loadMore: document.querySelector('.load-more'),
 };
 
 ref.form.addEventListener('submit', addImagesFetch);
 
-new SimpleLightbox('.gallery a')
-lightbox.refresh()
-
  async function addImagesFetch(e) {
     e.preventDefault();
-    ref.gallery.innerHTML = '';
+    clearArticlesContainer();
 
     try {
+      page = 1;
         const inputValue = ref.input.value;
-        const resultFetch = await fetchImages(inputValue);
+        const resultFetch = await fetchImages(inputValue, page);
         if (resultFetch.total <= 0) {
             onError()
             return;
         }
-       
+console.log(resultFetch);
         const render = await renderCardImages(resultFetch);
-        return ref.gallery.insertAdjacentHTML('beforeend', render);
+        Notify.success(`Hooray! We found ${resultFetch.totalHits} images.`);
+        ref.loadMore.style.display = 'block';
+        return ref.gallery.insertAdjacentHTML('beforeend',  render);
     } catch {onError()}
+};
+
+function clearArticlesContainer() {
+  ref.gallery.innerHTML = '';
 };
 
 function onError() {
         Notify.failure("Sorry, there are no images matching your search query. Please try again.", {
             position: 'center-center',
         });
+        ref.loadMore.style.display = 'none';
 };
 
-async function renderCardImages(name) {
-    return name.hits
-    .map(hit => {
-        const  { webformatURL, largeImageURL, tags, likes, views, comments, downloads} = hit;
+
+async function renderCardImages({ hits }) {
+    return hits.map(hit => {
+      const  { webformatURL, largeImageURL, tags, likes, views, comments, downloads} = hit;
 
       return `<div class="photo-card">
-      <a><img src="${webformatURL}" class="gallery__image" alt="${tags}" loading="lazy"></a>
+      <a class="open-original-photo"><img src="${webformatURL}" class="gallery__image" alt="${tags}" loading="lazy"></a>
       <div class="info">
         <p class="info-item">
           <b>Likes: ${likes}</b>
@@ -65,3 +76,29 @@ async function renderCardImages(name) {
     })
     .join('');
 };
+
+ref.loadMore.addEventListener('click', pageRenderCard)
+
+
+async function  pageRenderCard() {
+  try {
+    page +=1;
+    const inputValue = ref.input.value;
+    const renderNextPage = await fetchImages(inputValue, page);
+    const render = await renderCardImages(renderNextPage);
+    reachedEndResult(renderNextPage)
+    return ref.gallery.insertAdjacentHTML('beforeend',  render);
+  } catch  {
+    Notify.info("We're sorry, but you've reached the end of search results.", {position: 'center-bottom', timeout: 1000});
+  }
+};
+
+function reachedEndResult(name) {
+  let totalPages = Math.ceil(name.totalHits / perPage);
+if (page >= totalPages) {
+  Notify.info("We're sorry, but you've reached the end of search results.", {position: 'center-bottom', timeout: 3000});
+  ref.loadMore.style.display = 'none';
+  return
+};
+return
+}
--- src/search_images-api.js
+++ src/search_images-api.js
@@ -4,8 +4,8 @@ const api_key = axios.defaults.headers.common["x-api-key"] = "live_MXi5doRNd1frI
 const PIX_API_KEY = "39684416-19726dcb7b6323782764f8c99";
 const BASE_URL = 'https://pixabay.com/api/';
 
-async function fetchImages(searchTerm) {
-    const url = `${BASE_URL}?key=${PIX_API_KEY}&q=${searchTerm}&image_type=photo&orientation=horizontal&safesearch=true`;
+async function fetchImages(searchTerm, page) {
+    const url = `${BASE_URL}?key=${PIX_API_KEY}&q=${searchTerm}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
 
     const resolve = await fetch(url);
     return await resolve.json();
